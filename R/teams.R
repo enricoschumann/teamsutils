2c4f43c0ab72d4473f89029d655c1b8de56da53e
Enrico Schumann <es@enricoschumann.net>
2021-05-03 17:03:54 +0200

    Initial commit
    Provide functions 'send_message_card' and 'message_card'.

---
+++ R/teams.R
@@ -0,0 +1,27 @@
+send_message_card <- function(wh, msg, ...) {
+    res <- httr::POST(url = wh,
+                      httr::add_headers("Content-Type"="application/json"),
+                      body = msg,
+                      timeout = 3,
+                      encode = 'json'
+                      )
+
+    if(res$status_code == 200)
+        TRUE
+    else
+        res
+}
+
+message_card <- function(text, title, ...) {
+    msg <-
+'{
+  "@context": "https://schema.org/extensions",
+  "@type": "MessageCard",
+  "themeColor": "0072C6",
+  "title": "{title}",
+  "text": "{text}"
+}'
+    textutils::fill_in(msg,
+                       text = text,
+                       title = title)
+}
